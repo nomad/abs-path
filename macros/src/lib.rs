e54be6f3cc8a54e56117f1c95dc9c882975b1365
Riccardo Mazzarini <me@noib3.dev>
2025-03-16 17:39:04 +0100

    Implement `node!` and `path!`

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,1 +1,31 @@
-//! TODO: docs.
+//! .
+
+use abs_path_core::{AbsPath, NodeName};
+use proc_macro::TokenStream;
+use syn::{LitStr, parse_macro_input};
+
+/// TODO: docs.
+#[proc_macro]
+pub fn node(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as LitStr);
+    match <&NodeName>::try_from(&*input.value()) {
+        Ok(_) => quote::quote! {
+            unsafe { ::abs_path::NodeName::from_str_unchecked(#input) }
+        },
+        Err(err) => syn::Error::new_spanned(input, err).into_compile_error(),
+    }
+    .into()
+}
+
+/// TODO: docs.
+#[proc_macro]
+pub fn path(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as LitStr);
+    match <&AbsPath>::try_from(&*input.value()) {
+        Ok(_) => quote::quote! {
+            unsafe { ::abs_path::AbsPath::from_str_unchecked(#input) }
+        },
+        Err(err) => syn::Error::new_spanned(input, err).into_compile_error(),
+    }
+    .into()
+}
